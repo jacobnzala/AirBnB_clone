ede74bb8b8bd894b803b60c298db44a9bdcf5be1
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-08-07 16:53:20 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
- class Review which inherits from BaseModel
+ class Review that inherits from BaseModel
 """
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ define a User class """
+    """ defining User class """
     place_id = ""
     user_id = ""
     text = ""
