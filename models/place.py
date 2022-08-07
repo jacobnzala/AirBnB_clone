7d415b6366ec38054ea3d615abfac191464b4813
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-08-07 16:52:44 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,24 +1,22 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
-  modules
-  
+    Place modules
 """
 from models.base_model import BaseModel
 
-class place(basemodel):
-  """
-    this place calss will inherit from
-  """
-  
-  city_id = ""
-  user_id = ""
-  name = ""
-  description = ""
-  number_rooms = 0
-  number_bathrooms = 0
-  max_guest = 0
-  price_by_night = 0
-  latitude = 0.0
-  longitude = 0.0
-  amenity_ids = []
-  
+
+class Place(BaseModel):
+    """
+        Place Class inherit from base
+    """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
